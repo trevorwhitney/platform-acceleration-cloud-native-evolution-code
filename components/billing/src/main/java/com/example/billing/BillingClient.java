03dae5f8351a3a29dbd020dfe663b0aeecbb4fc1
Trevor Whitney <twhitney@pivotal.io>
2017-01-17 14:58:11 -0700

    my v4 solution

--- components/billing/src/main/java/com/example/billing/BillingClient.java
+++ components/billing/src/main/java/com/example/billing/BillingClient.java
@@ -1,8 +1,13 @@
 package com.example.billing;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.client.RestTemplate;
 
 public class BillingClient {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     private final RestTemplate restTemplate;
 
@@ -10,11 +15,16 @@ public class BillingClient {
         this.restTemplate = restTemplate;
     }
 
+    @HystrixCommand(fallbackMethod = "doNothing")
     public void billUser(String userId, Integer amountToBill) {
         restTemplate.postForEntity(
                 "//billing/reocurringPayments",
                 amountToBill,
                 String.class
         );
+    }
+
+    public void doNothing(String userId, Integer amountToBill) {
+        logger.error("Failed to post reocurring payment to billing service");
     }
 }
